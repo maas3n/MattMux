11659f0e45519737fb5010acdaa8eddea0eb48f0
maas3n <291940012+maas3n@users.noreply.github.com>
2026-09-11 00:15:41 +0200

    Bump ChromeOS build to Alpha 3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.maas3n.mattmux"
         minSdk = 26
         targetSdk = 36
-        versionCode = 102
-        versionName = "1.2.0-chromeos-alpha2"
+        versionCode = 103
+        versionName = "1.2.0-chromeos-alpha3"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
